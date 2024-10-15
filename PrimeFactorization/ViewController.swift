a09ae8ebf04bc8efc6b3f25e154ea94a3c45c820
stefanocasafranca <scasafrancal01@gmail.com>
2024-10-15 09:08:52 -0500

    Initial Commit

---
+++ PrimeFactorization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeFactorization
+//
+//  Created by Stefano Casafranca Laos on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
